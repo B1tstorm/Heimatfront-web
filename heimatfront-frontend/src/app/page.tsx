c502870f81a1a459720ad9712d4d167477d7510f
B1storm <you@example.com>
2025-03-04 22:29:39 +0100

    First version of fronend

--- heimatfront-frontend/src/app/page.tsx
+++ heimatfront-frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
+import Image from 'next/image'
 import styles from "./page.module.css";
 
-function getCurrentDateString(): string {
+function getCurrentYearString(): string {
   return new Date().getFullYear().toString();
 }
 
@@ -8,12 +9,29 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <p>Willkommen Kamerad, bei Heimatfront, einer der größten deutschen Communities für Arma Reforger</p>
+        <h1>Willkommen bei <div style={{color: "green"}}>Heimatfront</div> Kamerad</h1><h2>einer der größten deutschen Communities für Arma Reforger</h2>
       </main>
       <footer className={styles.footer}>
-        <pre>Die Seite befindet sich momentan im Aufbau</pre>
-        <pre><a href="https://github.com/B1tstorm">Developers GitHub</a></pre>
-        <pre>{getCurrentDateString()}</pre>
+        <pre>
+          <a href="https://ko-fi.com/heimatfront">Spenden
+            <Image src="/kofi_symbol.png" alt='Official Ko-fi logo' width={30} height={30} />
+          </a>
+        </pre>
+        <pre>
+          <a href="https://discord.gg/Es8RVSNW">Discord
+            <Image src="/Discord-Symbol-Blurple.png" alt='Official Discord logo' width={35} height={30} />
+          </a>
+        </pre>
+        <pre>
+          <a href="https://github.com/B1tstorm/Heimafront-web/">Projekt
+            <Image src="/github-mark.png" alt='Official GitHub logo' width={30} height={30} />
+          </a>
+        </pre>
+        <pre>
+          <a href="https://discord.gg/Es8RVSNW">Dev Kontakt
+            <Image src="https://avatars.githubusercontent.com/u/51802149?v=4" alt='Picture of the author' width={30} height={30} />{getCurrentYearString()}
+          </a>
+        </pre>
       </footer>
     </div>
   );
