227f67b9e1fbcf2c32cdf4de65db2438bebb36e1
B1storm <you@example.com>
2025-03-08 11:48:42 +0100

    Refactor NewsService to improve type definitions and enhance fetchNews method

--- src/app/news/NewsService.ts
+++ src/app/news/NewsService.ts
@@ -1,27 +1,28 @@
-import { Collection } from "mongodb";
+import { Collection, WithId } from "mongodb";
 import { MongoConnector } from "../MongoConnector";
 import { NewsItem, NewsItemDoc } from "./types";
 
 
 export interface INewsService {
-    fetchNews: () => Promise<any[]>
+    fetchNews: () => Promise<NewsItemDoc[]>
     addNews: (newsItem: NewsItem) => Promise<void>
 }
 
 export default class NewsService implements INewsService {
     private db: MongoConnector;
-    private news: Collection<Partial<NewsItemDoc>>;
+    private news: Collection<NewsItemDoc | NewsItem>;
 
     constructor() {
         this.db = new MongoConnector();
         this.news = this.db.getCollection('news');
     }
 
-    async fetchNews(): Promise<any[]> {
+    async fetchNews(): Promise<NewsItemDoc[]> {
         try {
-            const newsDocs = await this.news.find({}).toArray();
-
-            return newsDocs.map((doc) => doc);
+            return (await this.news
+                .find({})
+                .toArray())
+                .map((doc) => doc);
         } finally {
             await this.db.close();
         }
@@ -35,9 +36,3 @@ export default class NewsService implements INewsService {
         }
     }
 }
-
-// await addNews({
-//   title: "Mein zweites item",
-//   text: "Läuft würde ich sagen!",
-//   createdAt: new Date(),
-// });
